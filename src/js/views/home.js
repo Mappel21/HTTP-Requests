55f633c98ce4815ab46e78d7f8f010cba4e7b611
Mappel21 <Mappel7676@gmail.com>
2021-07-26 22:37:52 +0000

    adding todo list

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+//create your first component
+export function Home() {
+	const [todo, setTodo] = useState("");
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>To-Do List</h1>
+			<input
+				value={todo}
+				className="input"
+				placeholder="What needs to be done?"
+				onChange={e => setTodo(e.target.value)}
+			/>
+			<button
+				onClick={() => {
+					// setList([...list, todo]);
+					actions.addTodo(todo);
+					setTodo("");
+				}}
+				className="btn btn-success m-2">
+				Add
+			</button>
+			{store.todoList.map((item, index) => (
+				<div key={index}>
+					{item}
+					<button onClick={() => actions.deleteItem(store.todoList.filter(word => word !== item))}>x</button>
+				</div>
+			))}
+			{/* <footer>{list.length + " items left"}</footer> */}
+		</div>
+	);
+}
