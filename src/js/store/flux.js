ecd1adf4d890f2e6f32908c135b3500067f50697
Mappel21 <Mappel7676@gmail.com>
2021-07-28 20:33:49 +0000

    setting delete

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,11 +12,22 @@ const getState = ({ getStore, getActions, setStore }) => {
 					.then(response => getStore(setStore({ todoList: response })));
 			},
 			addTodo: todo => {
-				todo = [...getStore().todoList, todo];
-				console.log("todo", todo);
+				let list = getStore().todoList;
+				let newItem = [{ label: todo, done: false }];
+				list = [...list, ...newItem];
+				console.log("todo", list);
 				fetch("https://assets.breatheco.de/apis/fake/todos/user/mappel21", {
 					method: "PUT",
-					body: JSON.stringify(todo),
+					body: JSON.stringify(list),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}).then(() => getActions().getTodo());
+			},
+			deleteItem: array => {
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/mappel21", {
+					method: "PUT",
+					body: JSON.stringify(array),
 					headers: {
 						"Content-Type": "application/json"
 					}
