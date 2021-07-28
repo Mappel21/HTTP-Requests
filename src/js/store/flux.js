79869ddfaf6b1b9bcb4f8c5612742993c1198831
Mappel21 <Mappel7676@gmail.com>
2021-07-28 16:46:35 +0000

    modifying get

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,11 +6,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 				checkItem: ""
 		},
 		actions: {
-			getTodo: { =>{
-				fetch('https://assets.breatheco.de/apis/fake/todos/user/mappel21').then
+			getTodo: () =>{
+				fetch('https://assets.breatheco.de/apis/fake/todos/user/mappel21')
+				.then(res => res.json())
+				.then(response => setStore({ todos: response }));
 			},
 			addTitle: title =>(title === "" ? setStore({listTitle: "No title"}) : setStore({ listTitle: title}))
-			addItem
+			
 		}
 	};
 };
